efa4331ded0c1d46d43400e8f40ab1c3ace3c45c
Chippalrus <linlin.tart@gmail.com>
2015-09-26 04:30:42 -0700

    Create Enum.php

---
+++ Enum.php
@@ -0,0 +1,20 @@
+<?php
+//=========================================================================================
+//	Enum by Chippalrus
+//=========================================================================================
+/*
+	I Was using Enum here before, now I don't.
+	I don't want to change the files name though
+*/
+
+// URI for GW2API and Cache folders
+abstract class	EURI
+{
+	const	BASE				=	'https://api.guildwars2.com/v2/';
+	const	ACCESS_TOKEN		=	'?access_token=';
+	const	ITEMS			=	'items/';
+	const	TRAITS			=	'traits/';
+	const	SPECIALIZATIONS	=	'specializations/';
+	const	CHARACTERS		=	'characters/';
+}
+?>
